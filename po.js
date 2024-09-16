09be2e5ca4a59f8f9ea77f62306f2ba13611a55a
segmentJupyter <mesumana6@gmail.com>
2024-09-17 00:16:48 +0530

    Add files via upload

---
+++ po.js
@@ -0,0 +1,11 @@
+// You can add interactivity here, for example smooth scroll, animations, etc.
+// Example: Smooth scroll when clicking nav links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
